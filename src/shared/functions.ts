dbdeb21fb7e2c5202b3e7838a3ed87dcef1b71cf
Iaan Lamanosau <iaan.lamanosau@gmail.com>
2022-09-04 12:02:28 +0300

    Feat: Write shared password functions

--- src/shared/functions.ts
+++ src/shared/functions.ts
@@ -1,5 +1,5 @@
-import logger from 'jet-logger';
-
+import logger from "jet-logger";
+import bcrypt from "bcrypt";
 
 /**
  * Print an error object if it's truthy. Useful for testing.
@@ -15,4 +15,18 @@ export function pErr(err?: Error): void {
  */
 export function getRandomInt(): number {
   return Math.floor(Math.random() * 1_000_000_000_000);
+}
+
+/**
+ * @returns hashed password with salt
+ */
+export async function getHashedPassword(password: string): Promise<string> {
+  return await bcrypt.hash(password, 10);
+}
+
+export async function comparePassword(
+  password: string,
+  hash: string
+): Promise<boolean> {
+  return await bcrypt.compare(password, hash);
 }
